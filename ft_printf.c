52d8c751383c6b25525a3f8928ddf8c9582419ea
Anthony Torres Hernandez <anthtorr@car15s3.42barcelona.com>
2023-10-30 14:52:21 +0100

    adding new changes

--- ft_printf.c
+++ ft_printf.c
@@ -6,98 +6,281 @@
 /*   By: anthtorr <anthtorr@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/11 15:33:07 by anthtorr          #+#    #+#             */
-/*   Updated: 2023/10/15 16:14:00 by anthtorr         ###   ########.fr       */
+/*   Updated: 2023/10/30 14:52:06 by anthtorr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdarg.h>
+#include <stdlib.h>
 #include <unistd.h>
-
-int ft_putchar(char c) {
-    return write(1, &c, 1);
+/*
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
 }
 
-int ft_putstr(char *str) {
-    int len = 0;
-    while (str[len])
-        len++;
-    return write(1, str, len);
+void	ft_putstr(const char *str)
+{
+	while (*str)
+	{
+		ft_putchar(*str);
+		str++;
+	}
 }
 
-int ft_putnbr(int n) {
-    if (n == -2147483648)
-        return write(1, "-2147483648", 11);
+void	ft_putnbr(int num)
+{
+	if (num == -2147483648)
+	{
+		ft_putstr("-2147483648");
+		return ;
+	}
+	if (num < 0)
+	{
+		ft_putchar('-');
+		num = -num;
+	}
+	if (num >= 10)
+	{
+		ft_putnbr(num / 10);
+	}
+	ft_putchar(num % 10 + '0');
+}
 
-    if (n < 0) {
-        ft_putchar('-');
-        n = -n;
+void	ft_putunbr(unsigned int num)
+{
+	unsigned int	temp;
+	int				count;
+	char			*digitos;
+	int				indice;
+
+	if (num == 0)
+	{
+		ft_putchar('0');
+		return ;
+	}
+	temp = num;
+	count = 0;
+	while (temp > 0)
+	{
+		temp /= 10;
+		count++;
+	}
+	digitos = (char *)malloc((count + 1) * sizeof(char));
+	if (digitos == NULL)
+	{
+		return ;
+	}
+	indice = count;
+	while (num > 0)
+	{
+		digitos[indice - 1] = num % 10 + '0';
+		num /= 10;
+		indice--;
+	}
+	digitos[count] = '\0';
+	ft_putstr(digitos);
+	free(digitos);
+}
+
+void	ft_puthex(unsigned long long int num, char hex_base)
+{
+	int						count;
+	unsigned long long int	temp;
+	char					*digitos;
+	int						indice;
+	int						digit;
+
+	count = 0;
+	temp = num;
+	if (temp == 0)
+	{
+		ft_putchar('0');
+		return ;
+	}
+	while (temp > 0)
+	{
+		temp /= 16;
+		count++;
+	}
+	digitos = (char *)malloc((count + 1) * sizeof(char));
+	if (digitos == NULL)
+	{
+		return ;
+	}
+	indice = count;
+	while (num > 0)
+	{
+		digit = num % 16;
+		if (digit < 10)
+			digitos[indice - 1] = digit + '0';
+		else
+			digitos[indice - 1] = digit - 10 + hex_base;
+		num /= 16;
+		indice--;
+	}
+	digitos[count] = '\0';
+	ft_putstr(digitos);
+	free(digitos);
+}
+*/
+
+void	ft_putfloat(double num)
+{
+	int negativo;
+    unsigned long long int parte_entera = (unsigned long long int)num;
+	
+    double parte_decimal = num - parte_entera;
+	
+    int *digitos_decimales = (int *)malloc(6 * sizeof(int));
+	
+    int i = 0;
+	
+    int j = 0;
+	
+	negativo = 0;
+    if (num < 0)
+    {
+        negativo = 1;
+        num = -num;
     }
 
-    if (n >= 10)
-        ft_putnbr(n / 10);
+    // Parte entera
+    unsigned long long int parte_entera = (unsigned long long int)num;
+    ft_putunbr(parte_entera);
 
-    return ft_putchar(n % 10 + '0');
+    // Parte decimal
+    ft_putchar('.');
+    double parte_decimal = num - parte_entera;
+
+    // Reservar memoria para almacenar los dígitos decimales
+    int *digitos_decimales = (int *)malloc(6 * sizeof(int));
+
+    if (digitos_decimales == NULL)
+    {
+        // Error de asignación de memoria
+        return;
+    }
+
+    // Calcular los dígitos decimales
+    int i = 0;
+    while (i < 6)
+    {
+        parte_decimal *= 10;
+        digitos_decimales[i] = (int)parte_decimal;
+        parte_decimal -= digitos_decimales[i];
+        i++;
+    }
+
+    // Imprimir los dígitos decimales
+    int j = 0;
+    while (j < 6)
+    {
+        ft_putchar(digitos_decimales[j] + '0');
+        j++;
+    }
+
+    // Liberar la memoria asignada
+    free(digitos_decimales);
+
+    // Restaurar el signo si el número era negativo
+    if (negativo)
+    {
+        ft_putchar('-');
+    }
 }
 
-void ft_puthex(unsigned int n, int uppercase) {
-    char base = (uppercase) ? 'A' : 'a';
-    if (n >= 16)
-        ft_puthex(n / 16, uppercase);
-
-    char digit = (n % 16 > 9) ? n % 16 + base - 10 : n % 16 + '0';
-    write(1, &digit, 1);
-}
-
-int ft_printf(const char *format, ...) {
+/*
+int ft_printf(const char *format, ...)
+{
     va_list args;
+    va_start(args, format);
+
     int count = 0;
     const char *ptr = format;
 
-    va_start(args, format);
-
-    while (*ptr) {
-        if (*ptr == '%') {
+    while (*ptr)
+    {
+        if (*ptr == '%')
+        {
             ptr++;
-            if (*ptr == 'c') {
-                count += ft_putchar(va_arg(args, int));
-            } else if (*ptr == 's') {
-                count += ft_putstr(va_arg(args, char *));
-            } else if (*ptr == 'p') {
-                count += write(1, "0x", 2);
-                ft_puthex((unsigned int)va_arg(args, void *), 1);
-                count += 8; // Longitud de "0x" + 8 caracteres hexadecimales
-            } else if (*ptr == 'd' || *ptr == 'i') {
-                count += ft_putnbr(va_arg(args, int));
-            } else if (*ptr == 'u') {
-                count += ft_putnbr(va_arg(args, unsigned int));
-            } else if (*ptr == 'x' || *ptr == 'X') {
-                ft_puthex(va_arg(args, unsigned int), (*ptr == 'X') ? 1 : 0);
-                count += 8; // Longitud de 8 caracteres hexadecimales
-            } else if (*ptr == 'f') {
-                // Manejo de %f (números de punto flotante)
-                double num_f = va_arg(args, double);
-                int int_part = (int)num_f;
-                count += ft_putnbr(int_part);
-                ft_putchar('.');
-                // Extraemos hasta 6 decimales (como máximo)
-                double decimal_part = num_f - int_part;
-                for (int i = 0; i < 6; i++) {
-                    decimal_part *= 10;
-                    int digit = (int)decimal_part;
-                    count += ft_putnbr(digit);
-                    decimal_part -= digit;
-                }
-            } else if (*ptr == '%') {
-                count += ft_putchar('%');
+            if (*ptr == 'c')
+            {
+                char c = va_arg(args, int);
+                ft_putchar(c);
+                count++;
             }
-        } else {
-            count += ft_putchar(*ptr);
+            else if (*ptr == 's')
+            {
+                const char *str = va_arg(args, const char *);
+                while (*str)
+                {
+                    ft_putchar(*str);
+                    str++;
+                    count++;
+                }
+            }
+            else if (*ptr == 'p')
+			{
+				void *ptr_arg = va_arg(args, void *);
+				unsigned long long int val = (unsigned long long int)ptr_arg;
+				ft_putstr("0x");
+				if (val >> 32)
+					ft_puthex(val >> 32, 'a');
+				ft_puthex(val & 0xFFFFFFFF, 'a'); 
+				count += 18;
+			}
+            else if (*ptr == 'd' || *ptr == 'i')
+            {
+                int num = va_arg(args, int);
+                ft_putnbr(num);
+                count++;
+            }
+            else if (*ptr == 'u')
+            {
+                unsigned int num = va_arg(args, unsigned int);
+                ft_putunbr(num);
+                count++;
+            }
+            else if (*ptr == 'x' || *ptr == 'X')
+            {
+                unsigned int num = va_arg(args, unsigned int);
+                char hex_base;
+                if (*((char *)ptr) == 'x')
+                    hex_base = 'a';
+                else
+                    hex_base = 'A';
+                ft_puthex(num, hex_base);
+                count++;
+            }
+			else if (*ptr == 'f')
+            {
+                double num = va_arg(args, double);
+                ft_putfloat(num);
+                count++;
+            }
+            else if (*ptr == '%')
+            {
+                ft_putchar('%');
+                count++;
+            }
+            else
+            {
+                ft_putchar('%');
+                count++;
+                ft_putchar(*ptr);
+                count++;
+            }
+        }
+        else
+        {
+            ft_putchar(*ptr);
+            count++;
         }
         ptr++;
     }
 
     va_end(args);
-
     return count;
 }
 
@@ -163,4 +346,4 @@ int main() {
 
     return 0;
 }
-
+*/
